6a054c3ed4aad4e3dee076bb5360b7e9ed8daede
Ben Wilson <ben@thelocust.org>
2016-12-31 16:29:37 -0500

    more code cleanups for phpcs

--- index.php
+++ index.php
@@ -18,7 +18,6 @@
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
-
 namespace CXSeries\CXSeriesPlugin;
 
 // If this file is accessed directly, then abort.
@@ -30,7 +29,8 @@ if ( ! defined( 'WPINC' ) ) {
 add_action( 'init', __NAMESPACE__ . '\create_races_post_type' );
 
 /* Widget: Upcoming Races */
-add_action( 'widgets_init', create_function( '', 'register_widget( __NAMESPACE__ . "\UpcomingRacesWidget" );' ) );
+add_action( 'widgets_init', function() { register_widget( __NAMESPACE__ . '\UpcomingRacesWidget' );
+} );
 
 /* Shortcode: Next Race */
 add_shortcode( 'next_race', __NAMESPACE__ . '\next_race_shortcode' );
@@ -84,7 +84,7 @@ function fetch_races( $number_of_races = 1 ) {
  * Echoes display_race_details of have a next race, if not, show
  *  "no upcoming races" message.
  */
-function display_next_race() : void {
+function display_next_race() {
 	wp_reset_postdata();
 	$the_query = fetch_races( 1 );
 	if ( $the_query->have_posts() ) :
@@ -93,7 +93,7 @@ function display_next_race() : void {
 		while ( $the_query->have_posts() && $post_count < $number_of_posts ) :
 			$the_query->the_post();
 			echo '<div class="cxseries_next_race">';
-			display_race_details();
+			   display_race_details();
 			echo '</div>';
 			$post_count++;
 			endwhile;
@@ -110,7 +110,7 @@ function display_next_race() : void {
  *
  * @param bool $title display title of the race.
  */
-function display_race_details( $title = true ) : void  {
+function display_race_details( $title = true ) {
 	global $post;
 
 	$meta_keys = array( 'dates','location','website','facebook','twitter','registration' );
@@ -142,7 +142,7 @@ function display_race_details( $title = true ) : void  {
 	<?php endif; ?>
 
 			<div class="cxseries_race_meta">
-				<span class="cxseries_date"><?php echo esc_html( ( ! isset( $custom_fields['dates'] )) ? get_the_date() : $custom_fields['dates'][0] ) ); ?></span>
+				<span class="cxseries_date"><?php echo esc_html( ( ! isset( $custom_fields['dates'] )) ? get_the_date() : $custom_fields['dates'][0] ); ?></span>
 
 	<?php if ( $location ) : ?>
 				&bull;  <span class="cxseries_location"><?php echo esc_html( $location ); ?></span>
@@ -243,7 +243,7 @@ class UpcomingRacesWidget extends \WP_Widget {
 		wp_reset_postdata();
 		$number_of_posts = ( ! empty( $instance['number_of_posts'] )) ? $instance['number_of_posts'] : 999;
 
-		echo '<div class="cxseries_upcoming_races">';
+		echo ' <div class="cxseries_upcoming_races">';
 
 		if ( isset( $instance['widget_title'] ) && ! empty( $instance['widget_title'] ) ) :
 			echo '<h2>' . esc_html( $instance['widget_title'] ) . '</h2>';
@@ -252,13 +252,13 @@ class UpcomingRacesWidget extends \WP_Widget {
 		$the_query = fetch_races( $number_of_posts );
 
 		if ( $the_query->have_posts() ) :
-
-			echo '<ul>';
-
+		?>
+      <ul>
+		<?php
 			$post_count = 0;
 
-			while ( $the_query->have_posts() && $post_count < $number_of_posts ) :
-				$the_query->the_post(); ?>
+		while ( $the_query->have_posts() && $post_count < $number_of_posts ) :
+			$the_query->the_post(); ?>
 
 					  <li>
 						<?php display_race_details(); ?>
@@ -269,17 +269,20 @@ class UpcomingRacesWidget extends \WP_Widget {
 				$post_count++;
 
 				endwhile;
-
-			echo '</ul>';
+?>
+      </ul>
+<?php
 			else :
-
-				echo '<p>No upcoming races. See ya next season!</p>';
-
+?>
+				<p>No upcoming races. See ya next season!</p>
+<?php
 			endif;
 
 			wp_reset_postdata();
+?>
+      </div>
+<?php
 
-			echo '</div>';
 	}
 }
 
@@ -318,10 +321,10 @@ function upcoming_races_shortcode( $atts = [] ) {
 	$args = array(
 
 	/*
-	'before_widget' => '<div class="box widget scheme-' . $scheme . ' ">',
-	'after_widget'  => '</div>',
-	'before_title'  => '<div class="widget-title">',
-	'after_title'   => '</div>',
+	'before_widget' => ' < div class = "box widget scheme-' . $scheme . ' " > ',
+	'after_widget'  => ' < / div > ',
+	'before_title'  => ' < div class = 'widget-title' > ',
+	'after_title'   => ' < / div > ',
 	*/
 
 	);
@@ -358,7 +361,7 @@ function race_details_shortcode( $attributes ) {
  */
 function bikereg_form_shortcode( $attributes, $event_id = null ) {
 	if ( is_numeric( $event_id ) ) :
-		$html = '<div class="' . echo esc_attr( $class ) . '" style="max -width:800px; margin:auto;" id="regWrapper"> ' .
+		$html = '<div class="" style="max -width:800px; margin:auto;" id="regWrapper">' .
 			'<script src="https:// www.bikereg.com/Scripts/athleteRegWidget.js"></script> ' .
 			'<iframe src="https://www.bikereg.com/' . esc_attr( $event_id ) . '?if=1" style="height:100%;width:100%;border-radius:7px;" frameBorder="0" id="regFrame"></iframe> ' .
 			'</div>';
@@ -387,7 +390,7 @@ function fetch_results( $tag = '2015' ) {
  *
  * Lists all results
  */
-function list_results() : void {
+function list_results() {
 	global $post;
 
 	$tags = get_tags(
@@ -395,7 +398,7 @@ function list_results() : void {
 		'get' => 'all',
 		'orderby' => 'name',
 		'order' => 'desc',
-		),
+		)
 	);
 	if ( $tags ) :
 		foreach ( $tags as $tag ) :
@@ -418,8 +421,8 @@ function list_results() : void {
 
 				endwhile;
 					echo '</ul>';
-					else :
-						echo '<p>No results posted!</p>';
+	else :
+		echo '<p>No results posted!</p>';
 					endif;
 
 					wp_reset_postdata();
@@ -434,19 +437,19 @@ function list_results() : void {
 	wp_reset_postdata();
 }
 
-/**
- * Create Races Post Type
- *
- * Create and register the races custom post time
- */
-function create_races_post_type() : void {
+	/**
+	 * Create Races Post Type
+	 *
+	 * Create and register the races custom post time
+	 */
+function create_races_post_type() {
 	register_post_type(
 		'races',
 		array(
 		'labels' => array(
-	        'name' => __( 'Races' ),
-		      'singular_name' => __( 'Race' ),
-	        ),
+		'name' => __( 'Races' ),
+		  'singular_name' => __( 'Race' ),
+		),
 		'supports' => array( 'title','editor' ),
 		'public' => true,
 		'has_archive' => false,
@@ -456,234 +459,234 @@ function create_races_post_type() : void {
 	);
 }
 
-// TODO move these to an init function, define as constants?
-$cxseries_race_labels = array(
+	// TODO move these to an init function, define as constants?
+	$cxseries_race_labels = array(
 	'Location' => 'location',
 	'Sponsored By' => 'sponsoredby',
-);
+	);
 
-$cxseries_race_links = array(
+	$cxseries_race_links = array(
 	'Registration Link' => 'registration_link',
 	'Website' => 'website_link',
 	'Facebook' => 'facebook_link',
 	'Twitter' => 'twitter_link',
-);
+	);
 
 
-/**
- * Race Metabox Setup
- *
- * Create all race post type metaboxes (labels and links)
- */
-function race_metabox_setup() : void {
-	global $cxseries_race_labels, $cxseries_race_links;
+	/**
+	 * Race Metabox Setup
+	 *
+	 * Create all race post type metaboxes (labels and links)
+	 */
+	function race_metabox_setup() {
+		global $cxseries_race_labels, $cxseries_race_links;
 
-	foreach ( $cxseries_race_labels as $label_name => $label_id ) :
-		add_meta_box(
-			'cxseries_race_' . $label_id,
-			$label_name,
-			__NAMESPACE__ . '\race_labels_metaboxes',
-			'races',
-			'normal',
-			'high'
-		);
-		endforeach;
+		foreach ( $cxseries_race_labels as $label_name => $label_id ) :
+			add_meta_box(
+				'cxseries_race_' . $label_id,
+				$label_name,
+				__NAMESPACE__ . '\race_labels_metaboxes',
+				'races',
+				'normal',
+				'high'
+			);
+			endforeach;
 
-	foreach ( $cxseries_race_links as $link_name => $link_id ) :
-		add_meta_box(
-			'cxseries_race_' . $link_id,
-			$link_name,
-			__NAMESPACE__ . '\race_links_metaboxes',
-			'races',
-			'normal',
-			'high',
-			array(
-			'link_name' => $link_name,
-			'link_id' => $link_id,
-			),
-		);
-		endforeach;
-}
-
-
-/**
- * Display Race Label Metaboxes
- *
- * Displays race label metaboxes
- *
- * @param obj    $post the post object.
- * @param string $label label for this metabox.
- */
-function race_labels_metaboxes( $post, $label ) {
-	wp_nonce_field( basename( __FILE__ ), $label['id'] . '_nonce' );
-
-	$value = get_post_meta( $post->ID, $label['id'], true ); ?>
-		<p>
-		<label for="<?php echo esc_attr( $label['id'] ); ?>"><?php echo esc_html( $label['title'] );?></label>
-		<br />
-		<input class="widefat" type="text" name="<?php echo esc_attr( $label['id'] ) ?>" id="<?php echo esc_attr( $label['id'] ); ?>" value="<?php echo esc_attr( $value ); ?>" size="30" />
-	  </p>
-	<?php
-
-}
-
-
-/**
- * Save Race Label Metaboxes
- *
- * Saves metabox information on update
- *
- * @param id  $post_id the id of the post.
- * @param obj $post post object info.
- */
-function save_label_metaboxes( $post_id, $post ) {
-	global $cxseries_race_labels;
-
-	foreach ( $cxseries_race_labels as $label_name => $label_id ) :
-
-		/* Verify the nonce before proceeding. */
-		if ( ! isset( $_POST[ 'cxseries_race_' . $label_id . '_nonce' ] ) || ! wp_verify_nonce( wp_unslash( $_POST[ 'cxseries_race_' . $label_id . '_nonce' ] ) , basename( __FILE__ ) ) ) {
-			return $post_id;
-		}
-
-		/* Get the post type object. */
-		$post_type = get_post_type_object( $post->post_type );
-
-		/* Check if the current user has permission to edit the post. */
-		if ( ! current_user_can( $post_type->cap->edit_post, $post_id ) ) {
-			return $post_id;
-		}
-
-		/* Get the posted data and sanitize it for use as an HTML class. */
-		$new_meta_value = (isset( $_POST[ 'cxseries_race_' . $label_id ] ) ? $_POST[ 'cxseries_race_' . $label_id ] : '');
-
-		/* Get the meta key. */
-		$meta_key = 'cxseries_race_' . $label_id;
-
-		/* Get the meta value of the custom field key. */
-		$meta_value = get_post_meta( $post_id, $meta_key, true );
-
-		/* If a new meta value was added and there was no previous value, add it. */
-		if ( $new_meta_value && '' == $meta_value ) {
-			add_post_meta( $post_id, $meta_key, $new_meta_value, true );
-		} /* If the new meta value does not match the old value, update it. */
-		elseif ( $new_meta_value && $new_meta_value != $meta_value ) {
-			update_post_meta( $post_id, $meta_key, $new_meta_value );
-		} /* If there is no new meta value but an old value exists, delete it. */
-		elseif ( '' == $new_meta_value && $meta_value ) {
-			delete_post_meta( $post_id, $meta_key, $meta_value );
-		}
-
-		endforeach;
-}
-
-
-/**
- * Display Race Link Metaboxes
- *
- * Displays race link metaboxes
- *
- * @param obj    $post the post object.
- * @param string $link the link for this metabox.
- */
-function race_links_metaboxes( $post, $link ) {
-	wp_nonce_field( basename( __FILE__ ), $link['id'] . '_nonce' );
-
-	$value = get_post_meta( $post->ID, $link['id'], true ); ?>
-		<p>
-		<label for="<?php echo esc_attr( $link['id'] ) ?>"><?php echo esc_html( $link['title'] ); ?></label>
-		<br />
-		<input class="widefat" type="text" name="<?php echo esc_attr( $link['id'] ) ?>" id="<?php echo esc_attr( $link['id'] ); ?>" value="<?php echo esc_attr( $value ); ?>" size="30" />
-	  </p>
-	<?php
-
-}
-
-
-/**
- * Save Race Link Metaboxes
- *
- * Saves metabox information on update
- *
- * @param id  $post_id the id of the post.
- * @param obj $post post object info.
- */
-function save_link_metaboxes( $post_id, $post ) {
-	global $cxseries_race_links;
-
-	foreach ( $cxseries_race_links as $link_name => $link_id ) :
-
-		/* Verify the nonce before proceeding. */
-		if ( ! isset( $_POST[ 'cxseries_race_' . $link_id . '_nonce' ] ) || ! wp_verify_nonce( $_POST[ 'cxseries_race_' . $link_id . '_nonce' ], basename( __FILE__ ) ) ) {
-			return $post_id;
-		}
-
-		/* Get the post type object. */
-		$post_type = get_post_type_object( $post->post_type );
-
-		/* Check if the current user has permission to edit the post. */
-		if ( ! current_user_can( $post_type->cap->edit_post, $post_id ) ) {
-			return $post_id;
-		}
-
-		/*
-		Get the posted data. */
-		// TODO validate for valid link.
-		$new_meta_value = (isset( $_POST[ 'cxseries_race_' . $link_id ] ) ? $_POST[ 'cxseries_race_' . $link_id ] : '');
-
-		/* Get the meta key. */
-		$meta_key = 'cxseries_race_' . $link_id;
-
-		/* Get the meta value of the custom field key. */
-		$meta_value = get_post_meta( $post_id, $meta_key, true );
-
-		/* If a new meta value was added and there was no previous value, add it. */
-		if ( $new_meta_value && '' == $meta_value ) {
-			add_post_meta( $post_id, $meta_key, $new_meta_value, true );
-		} /* If the new meta value does not match the old value, update it. */
-		elseif ( $new_meta_value && $new_meta_value != $meta_value ) {
-			update_post_meta( $post_id, $meta_key, $new_meta_value );
-		} /* If there is no new meta value but an old value exists, delete it. */
-		elseif ( '' == $new_meta_value && $meta_value ) {
-			delete_post_meta( $post_id, $meta_key, $meta_value );
-		}
-
-		endforeach;
-}
-
-/**
- * Show Future Posts
- *
- * Allows for future, published posts to be displayed
- *
- * @param array $posts array of posts.
- */
-function show_future_posts( $posts ) {
-	global $wp_query, $wpdb;
-	if ( is_single() && $wp_query->post_count == 0 ) {
-		$posts = $wpdb->get_results( $wp_query->request );
+		foreach ( $cxseries_race_links as $link_name => $link_id ) :
+			add_meta_box(
+				'cxseries_race_' . $link_id,
+				$link_name,
+				__NAMESPACE__ . '\race_links_metaboxes',
+				'races',
+				'normal',
+				'high',
+				array(
+				'link_name' => $link_name,
+				'link_id' => $link_id,
+				)
+			);
+			endforeach;
 	}
-	return $posts;
-}
 
-/**
- * Add Categories for Attachements
- *
- * Registers new taxonomy (categories) for attachments, needed for organizing
- *  our "results"
- */
-function add_categories_for_attachments() : void {
-	register_taxonomy_for_object_type( 'category', 'attachment' );
-}
 
-/**
- * Add Tags for Attachements
- *
- * Registers new taxonomy (tags) for attachments, needed for organizing
- *  our "results"
- */
-function add_tags_for_attachments() {
-	register_taxonomy_for_object_type( 'post_tag', 'attachment' );
-}
+	/**
+	 * Display Race Label Metaboxes
+	 *
+	 * Displays race label metaboxes
+	 *
+	 * @param obj    $post the post object.
+	 * @param string $label label for this metabox.
+	 */
+	function race_labels_metaboxes( $post, $label ) {
+		wp_nonce_field( basename( __FILE__ ), $label['id'] . '_nonce' );
+
+		$value = get_post_meta( $post->ID, $label['id'], true ); ?>
+			<p>
+			<label for="<?php echo esc_attr( $label['id'] ); ?>"><?php echo esc_html( $label['title'] );?></label>
+			<br />
+			<input class="widefat" type="text" name="<?php echo esc_attr( $label['id'] ) ?>" id="<?php echo esc_attr( $label['id'] ); ?>" value="<?php echo esc_attr( $value ); ?>" size="30" />
+		  </p>
+		<?php
+
+	}
+
+
+	/**
+	 * Save Race Label Metaboxes
+	 *
+	 * Saves metabox information on update
+	 *
+	 * @param id  $post_id the id of the post.
+	 * @param obj $post post object info.
+	 */
+	function save_label_metaboxes( $post_id, $post ) {
+		global $cxseries_race_labels;
+
+		foreach ( $cxseries_race_labels as $label_name => $label_id ) :
+
+			/* Verify the nonce before proceeding. */
+			if ( ! isset( $_POST[ 'cxseries_race_' . $label_id . '_nonce' ] ) || ! wp_verify_nonce( wp_unslash( $_POST[ 'cxseries_race_' . $label_id . '_nonce' ] ) , basename( __FILE__ ) ) ) {
+				return $post_id;
+			}
+
+			/* Get the post type object. */
+			$post_type = get_post_type_object( $post->post_type );
+
+			/* Check if the current user has permission to edit the post. */
+			if ( ! current_user_can( $post_type->cap->edit_post, $post_id ) ) {
+				return $post_id;
+			}
+
+			/* Get the posted data and sanitize it for use as an HTML class. */
+			$new_meta_value = (isset( $_POST[ 'cxseries_race_' . $label_id ] ) ? $_POST[ 'cxseries_race_' . $label_id ] : '');
+
+			/* Get the meta key. */
+			$meta_key = 'cxseries_race_' . $label_id;
+
+			/* Get the meta value of the custom field key. */
+			$meta_value = get_post_meta( $post_id, $meta_key, true );
+
+			/* If a new meta value was added and there was no previous value, add it. */
+			if ( $new_meta_value && '' == $meta_value ) {
+				add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+			} /* If the new meta value does not match the old value, update it. */
+			elseif ( $new_meta_value && $new_meta_value != $meta_value ) {
+				update_post_meta( $post_id, $meta_key, $new_meta_value );
+			} /* If there is no new meta value but an old value exists, delete it. */
+			elseif ( '' == $new_meta_value && $meta_value ) {
+				delete_post_meta( $post_id, $meta_key, $meta_value );
+			}
+
+			endforeach;
+	}
+
+
+	/**
+	 * Display Race Link Metaboxes
+	 *
+	 * Displays race link metaboxes
+	 *
+	 * @param obj    $post the post object.
+	 * @param string $link the link for this metabox.
+	 */
+	function race_links_metaboxes( $post, $link ) {
+		wp_nonce_field( basename( __FILE__ ), $link['id'] . '_nonce' );
+
+		$value = get_post_meta( $post->ID, $link['id'], true ); ?>
+			<p>
+			<label for="<?php echo esc_attr( $link['id'] ) ?>"><?php echo esc_html( $link['title'] ); ?></label>
+			<br />
+			<input class="widefat" type="text" name="<?php echo esc_attr( $link['id'] ) ?>" id="<?php echo esc_attr( $link['id'] ); ?>" value="<?php echo esc_attr( $value ); ?>" size="30" />
+		  </p>
+		<?php
+
+	}
+
+
+	/**
+	 * Save Race Link Metaboxes
+	 *
+	 * Saves metabox information on update
+	 *
+	 * @param id  $post_id the id of the post.
+	 * @param obj $post post object info.
+	 */
+	function save_link_metaboxes( $post_id, $post ) {
+		global $cxseries_race_links;
+
+		foreach ( $cxseries_race_links as $link_name => $link_id ) :
+
+			/* Verify the nonce before proceeding. */
+			if ( ! isset( $_POST[ 'cxseries_race_' . $link_id . '_nonce' ] ) || ! wp_verify_nonce( $_POST[ 'cxseries_race_' . $link_id . '_nonce' ], basename( __FILE__ ) ) ) {
+				return $post_id;
+			}
+
+			/* Get the post type object. */
+			$post_type = get_post_type_object( $post->post_type );
+
+			/* Check if the current user has permission to edit the post. */
+			if ( ! current_user_can( $post_type->cap->edit_post, $post_id ) ) {
+				return $post_id;
+			}
+
+			/*
+			Get the posted data. */
+			// TODO validate for valid link.
+			$new_meta_value = (isset( $_POST[ 'cxseries_race_' . $link_id ] ) ? $_POST[ 'cxseries_race_' . $link_id ] : '');
+
+			/* Get the meta key. */
+			$meta_key = 'cxseries_race_' . $link_id;
+
+			/* Get the meta value of the custom field key. */
+			$meta_value = get_post_meta( $post_id, $meta_key, true );
+
+			/* If a new meta value was added and there was no previous value, add it. */
+			if ( $new_meta_value && '' == $meta_value ) {
+				add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+			} /* If the new meta value does not match the old value, update it. */
+			elseif ( $new_meta_value && $new_meta_value != $meta_value ) {
+				update_post_meta( $post_id, $meta_key, $new_meta_value );
+			} /* If there is no new meta value but an old value exists, delete it. */
+			elseif ( '' == $new_meta_value && $meta_value ) {
+				delete_post_meta( $post_id, $meta_key, $meta_value );
+			}
+
+			endforeach;
+	}
+
+	/**
+	 * Show Future Posts
+	 *
+	 * Allows for future, published posts to be displayed
+	 *
+	 * @param array $posts array of posts.
+	 */
+	function show_future_posts( $posts ) {
+		global $wp_query, $wpdb;
+		if ( is_single() && $wp_query->post_count == 0 ) {
+			$posts = $wpdb->get_results( $wp_query->request );
+		}
+		return $posts;
+	}
+
+	/**
+	 * Add Categories for Attachements
+	 *
+	 * Registers new taxonomy (categories) for attachments, needed for organizing
+	 *  our "results"
+	 */
+	function add_categories_for_attachments() {
+		register_taxonomy_for_object_type( 'category', 'attachment' );
+	}
+
+	/**
+	 * Add Tags for Attachements
+	 *
+	 * Registers new taxonomy (tags) for attachments, needed for organizing
+	 *  our "results"
+	 */
+	function add_tags_for_attachments() {
+		register_taxonomy_for_object_type( 'post_tag', 'attachment' );
+	}
 
 ?>
